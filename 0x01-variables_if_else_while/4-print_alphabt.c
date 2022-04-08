a259703040611b1a6bb0f80e70ad66435921f9a8
RebeccaMesfin <rebecca.mesfin.179@gmail.com>
2022-04-08 02:14:11 -0700

    my commit message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#inlcude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success/corrrect)
+ */
+int main(void)
+{
+  char alphabet;
+  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    {
+      if (aplhabet == 'q')
+	continue;
+      else if (alphabet == 'e')
+	continue;
+      putchar(alphabet);
+    }
+  putchar('\n');
+  return (0);
+}
