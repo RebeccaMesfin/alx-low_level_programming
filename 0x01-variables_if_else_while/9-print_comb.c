11982d195a2ccd2565791d756b9fe4c7660ec520
RebeccaMesfin <rebecca.mesfin.179@gmail.com>
2022-04-08 04:57:30 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,14 @@
 int main(void)
 {
   int number;
-  for (number = 40; number <= 57; number++)
+  for (number = '0'; number <= '9'; number++)
     {
       putchar(number);
-      if (number == 57)
+      if (number != '9')
 	{
-	  break;
-	}
       putchar(',');
       putchar(' ');
+	}
     }
   putchar('\n');
   return (0);
