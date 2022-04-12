5b77c8233c994026feebc89049b2834bb31ee5da
RebeccaMesfin <rebecca.mesfin.179@gmail.com>
2022-04-11 23:49:26 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+	char beki[] = "_putchar";
+	int c;
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(beki[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
