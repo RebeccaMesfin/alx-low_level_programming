3991a845191f836c3943b9bbb94d1002de88a3a1
RebeccaMesfin <rebecca.mesfin.179@gmail.com>
2022-04-13 23:43:59 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/** 
+*
+* main - checks an infinite loop
+* return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinity loop incoming :(\n");
+i =0;
+/*
+* while (i < 10)
+* {
+* putchar(i);
+* }
+*/
+printf("Infinite loop avoided! \n/\n");
+return (0);
+}
+
